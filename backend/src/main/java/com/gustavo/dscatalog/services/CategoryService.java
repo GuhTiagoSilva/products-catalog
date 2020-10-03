dc893440f642966fa0357609932edacb247a9b8f
Gustavo Tiago <gustavo.tiago.gts@outlook.com>
2020-10-02 21:47:17 -0500

    Database seeding

--- backend/src/main/java/com/gustavo/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/gustavo/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.gustavo.dscatalog.entities.Category;
 import com.gustavo.dscatalog.repositories.CategoryRepository;
@@ -15,6 +16,7 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository categoryRepository;
 
+	@Transactional(readOnly = true)//avoiding the locking in the database
 	public List<Category> findAll() {
 		return categoryRepository.findAll();
 	}
