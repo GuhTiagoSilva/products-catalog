466913928bd998901c845f15f0fa6489779b1a34
Gustavo Tiago <gustavo.tiago.gts@outlook.com>
2020-10-04 13:38:29 -0500

    Pagination

--- backend/src/main/java/com/gustavo/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/gustavo/dscatalog/services/CategoryService.java
@@ -9,6 +9,8 @@ import javax.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,13 +28,9 @@ public class CategoryService {
 	private CategoryRepository categoryRepository;
 
 	@Transactional(readOnly = true) // avoiding the locking in the database
-	public List<CategoryDTO> findAll() {
-
-		return categoryRepository
-				.findAll()
-				.stream()
-				.map(x -> new CategoryDTO(x))
-				.collect(Collectors.toList());
+	public Page<CategoryDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Category> list = categoryRepository.findAll(pageRequest);
+		return list.map(x-> new CategoryDTO(x));
 	}
 
 	@Transactional(readOnly = true)
