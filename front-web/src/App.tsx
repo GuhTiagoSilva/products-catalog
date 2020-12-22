9c2139d642ae689bbbbe1f2d496adbe3d47e6e39
GuhTiagoSilva <gustavo.tiago.gts@outlook.com>
2020-12-21 23:22:07 -0300

    Creating Routes

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import './core/assets/styles/custom.scss'
 import './app.scss'
 import Navbar from './core/components/Navbar';
+import Routes from './Routes';
 const App = () => {
   return (
-      <Navbar/>
-    
+      <Routes/> // Putting the routes here in the app, we are saying that my application is basead in routes
     );
 }
 
