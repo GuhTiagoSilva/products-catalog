1319f7fcde708ebb6e6da6f36812561fbbe80e06
GuhTiagoSilva <gustavo.tiago.gts@outlook.com>
2021-01-02 16:56:34 -0300

    Add base admin form structure

--- front-web/src/core/components/Navbar/index.tsx
+++ front-web/src/core/components/Navbar/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './styles.scss'
-import {Link, NavLink} from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => (
     <nav className="row bg-primary main-nav">
@@ -13,13 +13,13 @@ const Navbar = () => (
         <div className="col-6 offset-2">
             <ul className="main-menu">
                 <li>
-                    <NavLink to="/" activeClassName="active" exact>HOME</NavLink>
+                    <NavLink to="/" exact>HOME</NavLink>
                 </li>
                 <li>
-                    <NavLink to="/products" activeClassName="active">CATÁLOGO</NavLink>
+                    <NavLink to="/products" >CATÁLOGO</NavLink>
                 </li>
                 <li>
-                    <NavLink to="/admin" activeClassName="active">ADMIN</NavLink>
+                    <NavLink to="/admin" >ADMIN</NavLink>
                 </li>
             </ul>
         </div>
