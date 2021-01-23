a088a247a8826138900440491bc8eb8a98fa0165
GuhTiagoSilva <gustavo.tiago.gts@outlook.com>
2021-01-23 17:04:48 -0300

    Testing new Respoitory

--- backend/src/main/java/com/gustavo/dscatalog/dto/UserUpdateDTO.java
+++ backend/src/main/java/com/gustavo/dscatalog/dto/UserUpdateDTO.java
@@ -5,5 +5,4 @@ import com.gustavo.dscatalog.services.validation.UserUpdateValid;
 @UserUpdateValid
 public class UserUpdateDTO extends UserDTO {
 	private static final long serialVersionUID = 1L;
-	
 }
