d519df6a5f6bf95eac9319e0be3a4342c8109528
GuhTiagoSilva <gustavo.tiago.gts@outlook.com>
2021-01-23 12:22:13 -0300

    Custom Constraint Validator

--- backend/src/main/java/com/gustavo/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/gustavo/dscatalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.gustavo.dscatalog.dto;
 
+import com.gustavo.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO {
 
 	private static final long serialVersionUID = 1L;
